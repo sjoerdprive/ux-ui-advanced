711e2e89578d46095dbca9e681b9a462efca4b0a
nayensk <463180@student.saxion.nl>
2020-01-27 19:48:39 +0100

    i cri everitim

---
+++ MainPage/LenaAssets/TurnOn.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOn : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject obj;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    bool bo = true;
+    public void Activate()
+    {
+        if (obj != null)
+        {
+            obj.SetActive(bo);
+            if (bo == true)
+            {
+                bo = false;
+            }
+            else
+            {
+                bo = true;
+            }
+        }
+    }
+}
